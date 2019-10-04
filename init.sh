35ee0660e29042982869c95656720177f3fa0d7a
Task Ohmori <tascii.oh@gmail.com>
2019-10-04 17:53:04 +0000

    fix .env

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 
 echo "INK_DIR=`pwd`/code/ink_compile" > .env
 echo "PUBLIC_DNS=$1" >> .env
-echo "REACT_APP_PUBLIC_DNS=\"$1\"" > code/django_web_front/.env
+echo "REACT_APP_PUBLIC_DNS=\"$1\"" > code/web_front/.env
