9f4ec3dd17f7a7ed8919020582e7e15724cb08ec
Task Ohmori <tascii.oh@gmail.com>
2019-10-16 17:45:06 +0000

    enable to download from chrome, firefox, and safari

--- code/web_front/src/DownloadButton.js
+++ code/web_front/src/DownloadButton.js
@@ -4,12 +4,34 @@ import Icon from '@material-ui/core/Icon'
 
 const DownloadButton = (props) => {
 	const [css,setCss] = useState();
-	const onDownload=()=>{
+	const onDownload = () => {
+		const mimeType = props.mimeType;
+		const name = props.name;
 		const data = props.data;
-		var link = document.createElement('a');
-		link.download = props.name;
-		link.href = URL.createObjectURL(new Blob([data],{type:props.mimeType}));
-		link.click();
+
+		var bom  = new Uint8Array([0xEF, 0xBB, 0xBF]);
+		var blob = new Blob([bom, data],{type : mimeType});	
+		var a = document.createElement('a');
+		a.download = name;
+		a.target = '_blank';
+
+		if (window.navigator.msSaveBlob) {
+			// for IE
+			window.navigator.msSaveBlob(blob, name)
+		} else if (window.URL && window.URL.createObjectURL) {
+			// for Firefox
+			a.href = window.URL.createObjectURL(blob);
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+		} else if (window.webkitURL && window.webkitURL.createObject) {
+			// for Chrome
+			a.href = window.webkitURL.createObjectURL(blob);
+			a.click();
+		} else {
+			// for Safari
+			window.open('data:' + mimeType + ';base64,' + window.Base64.encode(data), '_blank');
+		}
 	}
 	
 	useEffect(()=>{
