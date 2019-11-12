d3cf5dfc7b992acef266d928e6d409f58d9e9d44
Task Ohmori <tascii.oh@gmail.com>
2019-11-12 20:30:36 +0000

    follow latest ink

--- code/web_front/src/CodeTemplate.js
+++ code/web_front/src/CodeTemplate.js
@@ -1,48 +1,80 @@
-const codeTemplate = String.raw`#![cfg_attr(not(feature = "std"), no_std)]
+const codeTemplate = String.raw`#![feature(proc_macro_hygiene)]
+#![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_core::{
-    env::DefaultSrmlTypes,
-    memory::format,
-    storage,
-};
-use ink_lang::contract;
+use ink_core::storage;
+use ink_lang2 as ink;
 
-contract! {
-    #![env = DefaultSrmlTypes]
-    
-		struct Flipper {
+#[ink::contract(version = "0.1.0")]
+mod sample {
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    struct Sample {
+        /// Stores a single ${'`'}bool${'`'} value on the storage.
         value: storage::Value<bool>,
     }
 
-    impl Deploy for Flipper {
-        fn deploy(&mut self) {
-            self.value.set(false)
-        }
-    }
-
-    impl Flipper {
-        pub(external) fn flip(&mut self) {
-            *self.value = !*self.value;
+    impl Sample {
+        /// Constructor that initializes the ${'`'}bool${'`'} value to the given ${'`'}init_value${'`'}.
+        #[ink(constructor)]
+        fn new(&mut self, init_value: bool) {
+            self.value.set(init_value);
         }
 
-        pub(external) fn get(&self) -> bool {
-            env.println(&format!("Flipper Value: {:?}", *self.value));
+        /// Constructor that initializes the ${'`'}bool${'`'} value to ${'`'}false${'`'}.
+        ///
+        /// Constructors can delegate to other constructors.
+        #[ink(constructor)]
+        fn default(&mut self) {
+            self.new(false)
+        }
+
+        /// A message that can be called on instantiated contracts.
+        /// This one flips the value of the stored ${'`'}bool${'`'} from ${'`'}true${'`'}
+        /// to ${'`'}false${'`'} and vice versa.
+        #[ink(message)]
+        fn flip(&mut self) {
+            *self.value = !self.get();
+        }
+
+        /// Simply returns the current value of our ${'`'}bool${'`'}.
+        #[ink(message)]
+        fn get(&self) -> bool {
             *self.value
         }
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::Flipper;
+    /// Unit tests in Rust are normally defined within such a ${'`'}#[cfg(test)]${'`'}
+    /// module and test functions are marked with a ${'`'}#[test]${'`'} attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
 
-    #[test]
-    fn it_works() {
-        let mut flipper = Flipper::deploy_mock();
-        assert_eq!(flipper.get(), false);
-        flipper.flip();
-        assert_eq!(flipper.get(), true);
+        /// We test if the default constructor does its job.
+        #[test]
+        fn default_works() {
+            // Note that even though we defined our ${'`'}#[ink(constructor)]${'`'}
+            // above as ${'`'}&mut self${'`'} functions that return nothing we can call
+            // them in test code as if they were normal Rust constructors
+            // that take no ${'`'}self${'`'} argument but return ${'`'}Self${'`'}.
+            let sample = Sample::default();
+            assert_eq!(sample.get(), false);
+        }
+
+        /// We test a simple use case of our contract.
+        #[test]
+        fn it_works() {
+            let mut sample = Sample::new(false);
+            assert_eq!(sample.get(), false);
+            sample.flip();
+            assert_eq!(sample.get(), true);
+        }
     }
-}`
+}
+`
 
 export default codeTemplate;
+
